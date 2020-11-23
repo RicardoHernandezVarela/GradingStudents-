ec54280985afcc328cdd81c638d2ca3c7a736c8c
RicardoHernandezVarela <rhvarela011192@gmail.com>
2020-11-22 19:27:07 -0600

    gradingStudents funtion, not rounding grades yet

--- gradingStudents.py
+++ gradingStudents.py
@@ -1,5 +1,16 @@
+def gradingStudents(grades):
+  roundedGrades = []
+
+  for grade in grades:
+    if grade % 5 == 0 or grade < 38:
+      roundedGrades.append(grade)
+    else:
+      print('Round grades here!!!')
+
+  return roundedGrades
+
 def main(): 
-    print('MAIN main')
+    print('Grading Students script')
 
     totalGrades = int(input('Enter the total grades tha will be rounded: '))
 
@@ -9,7 +20,11 @@ def main():
         newGrade = int(input('Enter a grade between 0 and 100: '))
         gradesToRound.append(newGrade)
 
-    print(gradesToRound)
+    print('INPUT GRADES: ', gradesToRound)
+
+    result = gradingStudents(gradesToRound)
+
+    print('OUTPUT GRADES: ', result)
 
 # Call main() function.
 if __name__ == '__main__':
