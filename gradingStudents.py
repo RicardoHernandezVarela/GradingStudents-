6b8baae237c589dbd917655b350909e711fa6caf
RicardoHernandezVarela <rhvarela011192@gmail.com>
2020-11-22 20:23:36 -0600

    Get students rounded grades

--- gradingStudents.py
+++ gradingStudents.py
@@ -11,6 +11,18 @@ def getMultiplesOfFive(num):
 
     return [multiple_2, multiple_1]
 
+def getRoundedGrade(multiple, grade):
+    rounded = grade
+
+    diff = multiple - grade
+
+    if diff < 3:
+        rounded = multiple
+    else:
+        rounded = grade
+  
+    return rounded
+
 def gradingStudents(grades):
     roundedGrades = []
 
@@ -18,7 +30,15 @@ def gradingStudents(grades):
         if grade % 5 == 0 or grade < 38:
             roundedGrades.append(grade)
         else:
-            print('Round grades here!!!')
+            multiples = getMultiplesOfFive(grade)
+            roundedNum = 0
+
+            if grade > multiples[0]:
+                roundedNum = getRoundedGrade(multiples[1], grade)
+            else:
+                roundedNum = getRoundedGrade(multiples[0], grade)
+            
+            roundedGrades.append(roundedNum)
 
     return roundedGrades
 
@@ -27,19 +47,17 @@ def main():
 
     totalGrades = int(input('Enter the total grades tha will be rounded: '))
 
-    # gradesToRound = []
+    gradesToRound = []
 
-    # for _ in range(0, totalGrades):
-    #     newGrade = int(input('Enter a grade between 0 and 100: '))
-    #     gradesToRound.append(newGrade)
+    for _ in range(0, totalGrades):
+        newGrade = int(input('Enter a grade between 0 and 100: '))
+        gradesToRound.append(newGrade)
 
-    # print('INPUT GRADES: ', gradesToRound)
+    print('INPUT GRADES: ', gradesToRound)
 
-    # result = gradingStudents(gradesToRound)
+    result = gradingStudents(gradesToRound)
 
-    # print('OUTPUT GRADES: ', result)
-
-    print(getMultiplesOfFive(totalGrades))
+    print('OUTPUT GRADES: ', result)
 
 # Call main() function.
 if __name__ == '__main__':
